cec9174ed706e791e76f72788aa8228284ff9bff
rui-rui-an <951929823@qq.com>
2020-12-21 21:14:09 +0800

    大事件完成

--- admin/libs/https.js
+++ admin/libs/https.js
@@ -21,6 +21,9 @@
         comment_pass:    baseURL + '/admin/comment/pass',//文章评论通过
         comment_reject:  baseURL + '/admin/comment/reject',//文章评论不通过
         comment_delete:  baseURL + '/admin/comment/delete',//文章评论删除
+        data_article:    baseURL + '/admin/data/article'  ,  //日新增文章
+        category:        baseURL + '/admin/data/category'  ,//各类型文章数量统计
+        article_info:    baseURL + '/admin/data/info'
     };
 
     //暴露接口
